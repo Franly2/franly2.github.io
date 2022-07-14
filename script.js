4a79d6a39e6aecc5f6178bf32671569b65056d09
franly2 <franlybudipramana588@gmail.com>
2022-07-14 20:57:50 +0700

    v7

--- script.js
+++
@@ -1,9 +0,0 @@
-private void removeRippleEffectFromCheckBox(CheckBox checkBox) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        Drawable drawable = checkBox.getBackground();
-        if (drawable instanceof RippleDrawable) {
-            drawable = ((RippleDrawable) drawable).findDrawableByLayerId(0);
-            checkBox.setBackground(drawable);
-        }
-    }
-}
